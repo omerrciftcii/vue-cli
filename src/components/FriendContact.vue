25384557064df5d0157bd6e4158d03e399681cb3
Ömer <omerrcftcc@gmail.com>
2023-09-06 10:22:53 +0300

    First commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,130 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }}
+    </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+      <li><strong>Email:</strong>{{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator: function (value) {
+        return value === "1" || value === "0";
+      },
+    },
+  },
+  emits: ["toggle-favorite"],
+  data() {
+    return {
+      detailsAreVisible: false,
+
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      //   this.isFavorite = !this.isFavorite;  // Bu parametre App.vue içerisinde manipüle edebilir, parametre olarak geçilen değişkenler compopnent içeriside değiştirilmez. Unidirection Data Flow
+
+      this.$emit("toggle-favorite", this.id);
+      this.friendIsFavorite = !this.friendIsFavorite;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <form action="submit" @submit.prevent="submitData">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" v-model="newFriend.name" placeholder="Name" />
+      </div>
+
+      <div>
+        <label for="email">Email</label>
+        <input
+          type="email"
+          v-model="newFriend.emailAddress"
+          placeholder="Email"
+        />
+      </div>
+      <div>
+        <label for="phone">Phone</label>
+        <input type="tel" v-model="newFriend.phoneNumber" placeholder="Phone" />
+      </div>
+      <div><button>Submit</button></div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      newFriend: {
+        id: "",
+        name: "",
+        emailAddress: "",
+        phoneNumber: "",
+      },
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("add-contact", this.newFriend);
+    },
+  },
+};
+</script>
+
